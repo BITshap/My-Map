915e894fa972d306d2ee3fbde1a8da6b48f1b06d
Nick Shapoff <nicholasshapoff@gmail.com>
2022-09-30 07:10:49 -0700

    Changed

--- file.js
+++ file.js
@@ -30,6 +30,17 @@ const UserMap = {
      .bindPopup("You are Here!")
      .openPopup(); 
 },
+
+    addMarkers(){
+    for (let i = 0; i < this.businesses.length; i++) {
+        this.markers = L.marker([
+            this.businesses[i].lat,
+            this.businesses[i].long,
+        ])
+        .bindPopup('{this.businesses[i].name}')
+        .addTo(this.map)
+    }
+},
 }
 // get coordinates via geolocation api
 
@@ -43,23 +54,56 @@ async function getLocation(){
 
 
 // get foursquare businesses
+async function gatherBusiness(business) {
+
+   const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'fsq3yc/2FLX9gRGo/lW54giARCQWBvKBlZwzTRiWaYsfNmk='
+     }
+   }
+  
+  let limit = 5
+  let lat = UserMap.coordinates[0]
+  let lon = UserMap.coordinates[1]
+  let response = await fetch('https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}', options)
+  let data = await response.text()
+  let parsedData = JSON.parse(data)
+  let businesses =  parsedData.results
+  return businesses
+
+}
 
 // process foursquare array
-
+function gatheringResults(data) {
+   let business = data.map((element) => {
+        let location = {
+            name: element.name,
+            lat: element.geocodes.main.latitude,
+            long: element.geocodes.main.longitude
+        };
+        return location
+   })
+   return business
+}
 // event handlers
 
 // window load
 window.onload = async () => { 
     const coords = await getLocation()
-    console.log(coords)
     UserMap.coordinates = coords
     UserMap.createMap()
 }
 
 
 // business submit button
-document.querySelectorAll('button').addeventlistner('click', async () => {
-    let UserBusiness = document.getElementById('business').value
-    UserMap.businesses = UserBusiness
+document.getElementById('business').addEventListener('click', async (event) => {
+    event.preventDefault(
+    let business = document.getElementById('business').value
+    let data = await gatherBusiness(business)
+    UserMap.businesses = gatheringResults(data)
+    UserMap.addMarkers()
+    console.log("clicked!")
 })
 
